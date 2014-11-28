e5ecfcf03f31684ea3f0d31def878cb28d9e6ac5
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2014-11-28 09:14:21 +0200

    New version: 1.0.1

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'FeedBack', 'fw' );
 $manifest['description'] = __( 'Adds the possibility to leave feedback (comments, reviews and rating) about your products, articles, etc. This replaces the default comments system.', 'fw' );
-$manifest['version'] = '1.0.0';
+$manifest['version'] = '1.0.1';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
