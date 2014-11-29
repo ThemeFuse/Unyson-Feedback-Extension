240ee6bac8446f9a2f174b9565371846cbfffd70
Sergiu Bagrin <sergey.roxigo@gmail.com>
2014-11-29 11:59:54 +0200

    Minor Changes in readme.md.php // Plugin Check

--- readme.md.php
+++ readme.md.php
@@ -12,23 +12,23 @@ This code is what displays the feedback on your website. Copy the following to y
 
 ##STEP 2
 
-###Paste the feedback code in your theme
+###Paste the feedback code in your theme
 
-Open Appearance/Editor and select <strong>single.php</strong> file to edit.
+Open Appearance/Editor and select <strong>single.php</strong> file to edit.
 
-In the theme, paste the code where you want your feedback to appear (usually beneath the the_title() tag) and then save your theme.
+In the theme, paste the code where you want your feedback to appear (usually beneath the the_title() tag) and then save your theme.
 
 ---
 
 ##STEP 3 (optional)
 
-###Add the feedback to your archive listings
+###Add the feedback to your archive listings
 
 Copy the following code to your clipboard:
 
 <code>&lt;?php if( function_exists('fw_ext_feedback') ) { fw_ext_feedback(); } ?&gt;</code>
 
-Open Appearance/Editor and select <strong>archive.php</strong> file to edit.
+Open Appearance/Editor and select <strong>archive.php</strong> file to edit.
 
 In the theme, find the place where each item is rendered and paste the code inside that code block.
 
