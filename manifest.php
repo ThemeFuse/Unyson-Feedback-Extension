ebebb2de33fe705e36f71dbf658f89bb0f20d172
Valeriu <valeriuzdrobau@gmail.com>
2014-12-05 16:14:29 +0200

    version 1.0.4

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'FeedBack', 'fw' );
 $manifest['description'] = __( 'Adds the possibility to leave feedback (comments, reviews and rating) about your products, articles, etc. This replaces the default comments system.', 'fw' );
-$manifest['version'] = '1.0.3';
+$manifest['version'] = '1.0.4';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
